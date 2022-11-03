9963ff894405815804754c0488360b9e60e62447
rachelpz <rachelpzro@gmail.com>
2022-11-03 18:01:21 -0400

    generalConfigs1.0

---
+++ src/main/java/cu/edu/cujae/backend/api/controller/UserController.java
@@ -0,0 +1,48 @@
+package cu.edu.cujae.backend.api.controller;
+
+import cu.edu.cujae.backend.core.dto.UserDto;
+import cu.edu.cujae.backend.core.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/")
+    public ResponseEntity<List<UserDto>> getUsers() throws SQLException {
+		List<UserDto> userList = userService.listUsers();
+        return ResponseEntity.ok(userList);
+    }
+	
+	@GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable String id) throws SQLException {
+		UserDto user = userService.getUserById(id);
+        return ResponseEntity.ok(user);
+    }
+	
+	@PostMapping("/")
+    public ResponseEntity<String> create(@RequestBody UserDto user) throws SQLException {
+		userService.createUser(user);
+        return ResponseEntity.ok("User Created");
+    }
+	
+	@PutMapping("/")
+    public ResponseEntity<String> update(@RequestBody UserDto user) throws SQLException {
+		//TODO code for update
+        return ResponseEntity.ok("User Updated");
+    }
+	
+	@DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable String id) throws SQLException {
+		userService.deleteUser(id);
+        return ResponseEntity.ok("User deleted");
+    }
+}
---
+++ src/main/java/cu/edu/cujae/backend/service/RoleServiceImpl.java
@@ -0,0 +1,39 @@
+package cu.edu.cujae.backend.service;
+
+import cu.edu.cujae.backend.core.dto.RoleDto;
+import cu.edu.cujae.backend.core.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+	@Autowired
+    private JdbcTemplate jdbcTemplate;
+	
+	@Override
+	public List<RoleDto> getRolesByUserId(String userId) throws SQLException {
+		List<RoleDto> roleList = new ArrayList<RoleDto>();
+		PreparedStatement pstmt = jdbcTemplate.getDataSource().getConnection().prepareStatement(
+			      "SELECT id, role_name, description FROM xrole inner join user_role on user_role.role_id = xrole.id where user_role.user_id = ?");
+
+		pstmt.setString(1, userId);
+
+		ResultSet rs = pstmt.executeQuery();
+		
+		while(rs.next()){
+			roleList.add(new RoleDto(rs.getLong("id")
+					,rs.getString("role_name")
+					,rs.getString("description")));
+		}
+		return roleList;
+	}
+
+}
