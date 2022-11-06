ef7b7fd14ee1d8ae86be5dbe66ea160bb5d83142
kaitozx0 <iamstraxer44@gmail.com>
2022-11-06 00:41:53 -0400

    setting up project

--- src/main/java/cu/edu/cujae/backend/service/UserServiceImpl.java
+++ src/main/java/cu/edu/cujae/backend/service/UserServiceImpl.java
@@ -21,10 +21,10 @@ import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
-	
+
 	@Autowired
     private JdbcTemplate jdbcTemplate;
-	
+
 	@Autowired
 	private RoleService roleService;
 
@@ -32,19 +32,19 @@ public class UserServiceImpl implements UserService {
 	public void createUser(UserDto user) throws SQLException {
 		CallableStatement CS = jdbcTemplate.getDataSource().getConnection().prepareCall(
 				"{call create_user(?, ?, ?, ?, ?, ?, ?)}");
-		
+
 		CS.setString(1, UUID.randomUUID().toString().replaceAll("-", "").substring(0, 9));
 		CS.setString(2, user.getUsername());
 		CS.setString(3, user.getFullName());
 		CS.setString(4, getMd5Hash(user.getPassword()));
 		CS.setString(5, user.getEmail());
 		CS.setString(6, user.getIdentification());
-		
+
 		//roles separados por coma, ej: "1, 2, 4"
 		String roles = user.getRoles().stream().map(r -> r.getId().toString()).collect(Collectors.joining(","));
 		CS.setString(7, roles);
-		CS.executeUpdate();	
-		
+		CS.executeUpdate();
+
 	}
 
 	@Override
@@ -52,7 +52,7 @@ public class UserServiceImpl implements UserService {
 		List<UserDto> userList = new ArrayList<UserDto>();
 		ResultSet rs = jdbcTemplate.getDataSource().getConnection().createStatement().executeQuery(
 				"SELECT * FROM xuser");
-		
+
 		while(rs.next()){
 			userList.add(new UserDto(rs.getString("id")
 					,rs.getString("username")
@@ -64,25 +64,25 @@ public class UserServiceImpl implements UserService {
 		}
 		return userList;
 	}
-	
+
 	@Override
 	public void updateUser(UserDto user) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public UserDto getUserById(String userId) throws SQLException {
-		
+
 		UserDto user = null;
-		
+
 		PreparedStatement pstmt = jdbcTemplate.getDataSource().getConnection().prepareStatement(
 			      "SELECT * FROM xuser where id = ?");
 
 		pstmt.setString(1, userId);
 
 		ResultSet rs = pstmt.executeQuery();
-		
+
 		while(rs.next()){
 			user = new UserDto(rs.getString("id")
 					,rs.getString("username")
@@ -92,20 +92,20 @@ public class UserServiceImpl implements UserService {
 					,rs.getString("identification")
 					,roleService.getRolesByUserId(rs.getString("id")));
 		}
-		
+
 		return user;
 	}
 
 	@Override
 	public void deleteUser(String userId) throws SQLException {
-		
+
 		CallableStatement CS = jdbcTemplate.getDataSource().getConnection().prepareCall(
 				"{call delete_user(?)}");
-		
+
 		CS.setString(1, userId);
-		CS.executeUpdate();	
+		CS.executeUpdate();
 	}
-	
+
 	private String getMd5Hash(String password) {
 		MessageDigest md;
 		String md5Hash = "";
